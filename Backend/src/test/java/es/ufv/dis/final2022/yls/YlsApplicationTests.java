1b720369742cb05f4832ee3dbde346e9a4ef49ee
YurenaLopez <7836067@alumnos.ufv.es>
2024-06-18 22:18:45 +0200

    first commit

---
+++ Backend/src/test/java/es/ufv/dis/final2022/yls/YlsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.final2022.yls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
