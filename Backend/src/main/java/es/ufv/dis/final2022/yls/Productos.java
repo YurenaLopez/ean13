4cbf5a6591b75e69820a36938e80e2814ac04e5e
YurenaLopez <7836067@alumnos.ufv.es>
2024-06-18 22:26:12 +0200

    se añade la clase productos

---
+++ Backend/src/main/java/es/ufv/dis/final2022/yls/Productos.java
@@ -0,0 +1,59 @@
+package es.ufv.dis.final2022.yls;
+
+public class Productos {
+    String nombre;
+    String categoria;
+    String precio;
+    String ean13;
+
+    public Productos() {
+    }
+
+    public Productos(String nombre, String categoria, String precio, String ean13) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.ean13 = ean13;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getEan13() {
+        return ean13;
+    }
+
+    public void setEan13(String ean13) {
+        this.ean13 = ean13;
+    }
+
+    @Override
+    public String toString() {
+        return "Productos{" +
+                "nombre='" + nombre + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", precio='" + precio + '\'' +
+                ", ean13='" + ean13 + '\'';
+    }
+}
